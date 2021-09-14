fdb5b5c46bb9f9fcf142158dfa79d20f41f4a70f
Brianna Bruce <bbruce2@zagmail.gonzaga.edu>
2021-09-13 20:10:41 -0700

    ch 2.10 more messages + better code

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -5,6 +5,8 @@
 //  Created by Brianna Bruce on 9/12/21.
 //
 
+
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,14 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     var imageNumber = 0
+    var messageNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
+        let messages = ["You Are Awesome!",
+                        "You Are Great",
+                        "You Are Fantastic",
+                        "Fabulous? That's You!"]
         
-        print(imageNumber)
+        messageLabel.text = messages[messageNumber]
+        messageNumber += 1
+        if messageNumber == messages.count {
+            messageNumber = 0
+        }
+        
         // let imageName = "image" + String(imageNumber)
         let imageName = "image\(imageNumber)"
         imageView.image = UIImage (named: imageName)
